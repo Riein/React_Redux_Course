93596e7609b9cbd5be8221a099442423ec1b5d52
Sasha Demyanik <ttsvan@msn.com>
2018-06-01 14:10:10 -0700

    Adds UserInput and UserOutput components.

---
+++ example-app/src/Components/UserOutput.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class UserOutput extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p id="paragraph1"></p>
+        <p id="paragraph2"></p>
+      </div>
+    );
+  }
+}
+
+export default App;
