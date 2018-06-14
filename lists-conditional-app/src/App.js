c1994e176fd9ee8b8b63b1e78a119a79011f85b4
Sasha Demyanik <ttsvan@msn.com>
2018-06-13 18:04:49 -0700

    Adds input and changeTextHandler

--- lists-conditional-app/src/App.js
+++ lists-conditional-app/src/App.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    value: "Hello World"
+  };
+
+  changeTextHandler = (event) => {
+    console.log(event.target.value);
+    this.setState({value: event.target.value});
+  }
+
   render() {
     return (
       <div className="App">
-        Hello World!
+        <input onChange={this.changeTextHandler} value={this.state.value} />
       </div>
     );
   }
