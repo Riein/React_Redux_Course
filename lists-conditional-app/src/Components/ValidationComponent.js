f04bb8cdead6123c1b5f7bc0477bddfaa5c9ac04
Sasha Demyanik <ttsvan@msn.com>
2018-06-14 21:27:17 -0700

    Passes the text length of value to ValidationComponent

--- lists-conditional-app/src/Components/ValidationComponent.js
+++ lists-conditional-app/src/Components/ValidationComponent.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ValidationComponent = (props) => {
     return (
         <div>
-
+            Current text length is: {props.textLength}
         </div>
     );
 };
