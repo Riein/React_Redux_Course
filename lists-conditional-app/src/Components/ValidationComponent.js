2828416378236bf642d88c8527874f78d0679096
Sasha Demyanik <ttsvan@msn.com>
2018-06-15 16:06:47 -0700

    Displays if text is too short or long enough.

--- lists-conditional-app/src/Components/ValidationComponent.js
+++ lists-conditional-app/src/Components/ValidationComponent.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 const ValidationComponent = (props) => {
+    let text = "";
+
+    if (props.textLength < 5) {
+        text = "Text too short";
+    } else {
+        text = "Text long enough";
+    }
+
     return (
         <div>
-            Current text length is: {props.textLength}
+            {text}
         </div>
     );
 };
