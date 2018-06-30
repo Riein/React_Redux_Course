83b5df6ba01a01589546e69e7c54f99e9d22af86
Sasha Demyanik <ttsvan@msn.com>
2018-06-29 23:47:52 -0700

    Adds Radium styling to Persons project.

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Radium from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -51,7 +52,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null;
@@ -72,12 +77,19 @@ class App extends Component {
       );
     }
 
+
+    style.backgroundColor = 'red';
+    style[':hover'] = {
+      backgroundColor: 'salmon',
+      color: 'black'
+    };
+
     let classes =[];
 
     if (this.state.persons.length <= 2) {
       classes.push('red');
     }
-    if (this.state.person.length <= 1) {
+    if (this.state.persons.length <= 1) {
       classes.push('bold');
     }
 
@@ -96,4 +108,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
