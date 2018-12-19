bf9190641ae24dfe5243ffce34ace5c4ed252a23
Sasha Demyanik <ttsvan@msn.com>
2018-12-18 16:27:12 -0800

    Adds in functionality to add/remove ingredients.

--- basic-burger-app/src/components/Burger/BuildControls/BuildControls.js
+++ basic-burger-app/src/components/Burger/BuildControls/BuildControls.js
@@ -13,7 +13,12 @@ const controls = [
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
         {controls.map(ctrl => (
-            <BuildControl key={ctrl.label} label={ctrl.label} />
+            <BuildControl 
+                key={ctrl.label} 
+                label={ctrl.label}
+                removed={() => props.ingredientRemoved(ctrl.type)} 
+                added={() => props.ingredientAdded(ctrl.type)}    
+            />
         ))}
     </div>
 );
