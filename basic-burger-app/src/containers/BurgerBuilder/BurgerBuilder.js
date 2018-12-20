116c848e8eaa584f5d1a05639702e314634cef93
Sasha Demyanik <ttsvan@msn.com>
2018-12-20 15:25:25 -0800

    Updates the purchase state and button.

--- basic-burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ basic-burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,7 +20,24 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0,
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false
+    }
+
+    updatePurchaseState (updatedIngredients) {
+        const ingredients = {
+            ...updatedIngredients
+        };
+        const sum = Object.keys(ingredients)
+            .map(igKey => {
+                return ingredients[igKey];
+            })
+            .reduce((sum, el) => {
+                return sum + el;
+            }, 0);
+
+        this.setState({purchasable: sum > 0});
+
     }
 
     addIngredientHandler = (type) => {
@@ -38,7 +55,7 @@ class BurgerBuilder extends Component {
         this.setState({ totalPrice: newPrice,
             ingredients: updatedIngredients
         });
-
+        this.updatePurchaseState(updatedIngredients);
     }
 
     removeIngredientHandler = (type) => {
@@ -59,6 +76,7 @@ class BurgerBuilder extends Component {
         this.setState({ totalPrice: newPrice,
             ingredients: updatedIngredients
         });
+        this.updatePurchaseState(updatedIngredients);
     }
 
     render () {
@@ -76,6 +94,7 @@ class BurgerBuilder extends Component {
                     ingredientRemoved={this.removeIngredientHandler} 
                     ingredientAdded={this.addIngredientHandler}
                     disabled={disabledInfo}
+                    purchasable={this.state.purchasable}
                     price={this.state.totalPrice}
                 />
             </Aux>
